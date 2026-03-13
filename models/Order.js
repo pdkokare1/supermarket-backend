f5ec6b38bb429964b1b2193987776ea7c78fae6f
pdkokare1 <pdkokare1@gmail.com>
2026-03-13 17:32:03 +0530

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-    customerName: { type: String, default: 'Guest Customer' }, // Temporary until Firebase is wired
+    customerName: { type: String, required: true },
+    customerPhone: { type: String, required: true },
+    deliveryAddress: { type: String, required: true },
     items: { type: Array, required: true },
     totalAmount: { type: Number, required: true },
     status: { type: String, default: 'Order Placed' },
