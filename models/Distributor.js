d808753087dc39d3df407dcf83fc63059a41c276
pdkokare1 <pdkokare1@gmail.com>
2026-03-17 17:43:20 +0530

    Create Distributor.js

---
+++ models/Distributor.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const distributorSchema = new mongoose.Schema({
+    name: { type: String, required: true, unique: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Distributor', distributorSchema);
