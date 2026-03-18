5102dec46982f265fe0ea0066ce13b5e0213e974
pdkokare1 <pdkokare1@gmail.com>
2026-03-18 22:47:33 +0530

    Create Customer.js

---
+++ models/Customer.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const customerSchema = new mongoose.Schema({
+    phone: { 
+        type: String, 
+        required: true,
+        unique: true
+    },
+    name: { 
+        type: String, 
+        required: true 
+    },
+    isCreditEnabled: { 
+        type: Boolean, 
+        default: false 
+    },
+    creditLimit: { 
+        type: Number, 
+        default: 0 
+    },
+    creditUsed: { 
+        type: Number, 
+        default: 0 
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Customer', customerSchema);
