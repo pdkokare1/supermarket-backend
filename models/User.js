4a019530c6d7efbe10ad0aa59efbf6d0497289d1
pdkokare1 <pdkokare1@gmail.com>
2026-03-26 10:56:56 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -12,8 +12,8 @@ const userSchema = new mongoose.Schema({
     },
     pin: { 
         type: String, 
-        required: true,
-        unique: true
+        required: true
+        // MODIFIED: Removed unique: true to prevent duplicate key errors on default PINs
     },
     role: { 
         type: String, 
@@ -28,7 +28,20 @@ const userSchema = new mongoose.Schema({
     tokenVersion: { 
         type: Number, 
         default: 0 
+    },
+    // --- NEW: Brute-Force Protection ---
+    failedLoginAttempts: {
+        type: Number,
+        default: 0
+    },
+    lockUntil: {
+        type: Date
     }
 }, { timestamps: true });
+
+// --- NEW: Virtual field to check if the account is currently locked ---
+userSchema.virtual('isLocked').get(function() {
+    return !!(this.lockUntil && this.lockUntil > Date.now());
+});
 
 module.exports = mongoose.model('User', userSchema);
