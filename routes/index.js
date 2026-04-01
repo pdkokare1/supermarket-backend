e2a8369529ccc19df9ded7a7616837bf34f60f7b
pdkokare1 <pdkokare1@gmail.com>
2026-04-01 12:44:17 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@
 const fs = require('fs');
 const path = require('path');
 
-async function apiRoutes(fastify, options) {
-    // Dynamically read all files in the current directory asynchronously
+function apiRoutes(fastify, options) {
+    // Dynamically read all files in the current directory synchronously
     const routesDir = __dirname;
-    const files = await fs.promises.readdir(routesDir);
+    const files = fs.readdirSync(routesDir);
 
     for (const file of files) {
         // Skip this index.js file and ensure it's a javascript file
