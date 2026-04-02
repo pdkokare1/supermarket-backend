2e6845d124a827a12456d3ceef252354178ec4cd
pdkokare1 <pdkokare1@gmail.com>
2026-04-02 18:29:53 +0530

    Create categoryController.js

---
+++ controllers/categoryController.js
@@ -0,0 +1,18 @@
+/* controllers/categoryController.js */
+
+const categoryService = require('../services/categoryService');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getCategories = catchAsync(async (request, reply) => {
+    return await categoryService.getAllCategories();
+}, 'fetching categories');
+
+exports.createCategory = catchAsync(async (request, reply) => {
+    const newCategory = await categoryService.createCategory(request.body.name);
+    
+    if (request.server.broadcastToPOS) {
+        request.server.broadcastToPOS({ type: 'CATEGORY_ADDED', categoryId: newCategory._id });
+    }
+
+    return { success: true, message: 'Category added', data: newCategory };
+}, 'creating category');v
