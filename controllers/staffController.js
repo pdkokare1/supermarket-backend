cf4ed593ad56d5f79d0b7e33ca442977fb2d8c17
pdkokare1 <pdkokare1@gmail.com>
2026-04-03 12:23:54 +0530

    Create staffController.js

---
+++ controllers/staffController.js
@@ -0,0 +1,14 @@
+/* controllers/staffController.js */
+
+const staffService = require('../services/staffService');
+const catchAsync = require('../utils/catchAsync');
+
+exports.createStaff = catchAsync(async (request, reply) => {
+    const staffData = await staffService.createStaff(request.body);
+    return { success: true, message: 'User created successfully.', data: staffData };
+}, 'during user creation');
+
+exports.getStaff = catchAsync(async (request, reply) => {
+    const staff = await staffService.getAllStaff();
+    return { success: true, data: staff };
+}, 'fetching staff');
