154bda15e20f267a2a365dbfaff8cd94ba147ff6
pdkokare1 <pdkokare1@gmail.com>
2026-04-10 19:39:47 +0530

    Create apiDocsSetup.js

---
+++ plugins/apiDocsSetup.js
@@ -0,0 +1,17 @@
+/* plugins/apiDocsSetup.js */
+'use strict';
+
+module.exports = function(fastify) {
+    fastify.register(require('@fastify/swagger'), {
+        swagger: {
+            info: { title: 'DailyPick API', description: 'Enterprise Backend API', version: '1.0.0' },
+            consumes: ['application/json'],
+            produces: ['application/json']
+        }
+    });
+    
+    fastify.register(require('@fastify/swagger-ui'), {
+        routePrefix: '/api/docs',
+        uiConfig: { docExpansion: 'none', deepLinking: false }
+    });
+};
