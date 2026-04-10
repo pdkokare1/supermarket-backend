276e9c7d3dcc93d0ce4fbe82eeb21cc6e9b39cc0
pdkokare1 <pdkokare1@gmail.com>
2026-04-10 21:56:48 +0530

    Update staffController.js

--- controllers/staffController.js
+++ controllers/staffController.js
@@ -6,10 +6,7 @@ const catchAsync = require('../utils/catchAsync');
 exports.createStaff = catchAsync(async (request, reply) => {
     const staffData = await staffService.createStaff(request.body);
     
-    // MODULARIZED: Alert real-time admin dashboards
-    if (request.server.broadcastToPOS) {
-        request.server.broadcastToPOS({ type: 'STAFF_CREATED', username: staffData.username });
-    }
+    // REMOVED: request.server.broadcastToPOS (Now handled by Service events)
 
     return { success: true, message: 'User created successfully.', data: staffData };
 }, 'during user creation');
