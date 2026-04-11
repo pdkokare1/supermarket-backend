490139c11c61d8ade71a80f162c678788b84c049
pdkokare1 <pdkokare1@gmail.com>
2026-04-11 14:58:11 +0530

    Update inventoryHandler.js

--- jobs/inventoryHandler.js
+++ jobs/inventoryHandler.js
@@ -1,11 +1,8 @@
 /* jobs/inventoryHandler.js */
 
+const cacheUtils = require('../utils/cacheUtils');
+
 exports.handleInventoryReport = async (redisClient, fastify, newReport) => {
-    if (redisClient) {
-        try {
-            await redisClient.set('cache:inventory:report', JSON.stringify(newReport), 'EX', 86400); // 24h cache
-        } catch(e) {
-            fastify.log.error('Failed to update inventory report in Redis:', e);
-        }
-    }
+    // OPTIMIZED: Replaced raw redis call with centralized cacheUtils
+    await cacheUtils.setCachedData('cache:inventory:report', newReport, 86400); 
 };
