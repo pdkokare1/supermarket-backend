d4557c110ff9558094247121f23612bd86f8a024
pdkokare1 <pdkokare1@gmail.com>
2026-04-11 12:11:05 +0530

    Update sseController.js

--- controllers/sseController.js
+++ controllers/sseController.js
@@ -3,16 +3,10 @@
 
 const sseService = require('../services/orderSseService');
 
-/**
- * Initializes the SSE stream for Admin panel notifications.
- */
 exports.streamAdmin = async (request, reply) => {
     sseService.initializeAdminStream(request, reply);
 };
 
-/**
- * Initializes the SSE stream for specific Customer tracking.
- */
 exports.streamCustomer = async (request, reply) => {
     sseService.initializeCustomerStream(request, reply, request.params.id);
 };
