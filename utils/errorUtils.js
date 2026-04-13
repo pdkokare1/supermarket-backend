3f58be1e8f8846bcaa7e30ca92bae0fc99356eaa
pdkokare1 <pdkokare1@gmail.com>
2026-04-13 12:40:13 +0530

    Update errorUtils.js

--- utils/errorUtils.js
+++ utils/errorUtils.js
@@ -3,6 +3,19 @@
 const AppError = require('./AppError'); 
 
 exports.handleControllerError = (request, reply, error, contextMessage) => {
+    // OPTIMIZATION: Intercept MongoDB-specific errors automatically to prevent unhandled rejections
+    if (error.name === 'CastError') {
+        return reply.status(400).send({ success: false, message: `Invalid ${error.path}: ${error.value}` });
+    }
+    if (error.name === 'ValidationError') {
+        const messages = Object.values(error.errors).map(val => val.message);
+        return reply.status(400).send({ success: false, message: `Invalid input data. ${messages.join('. ')}` });
+    }
+    if (error.code === 11000) {
+        const value = error.errmsg ? error.errmsg.match(/(["'])(\\?.)*?\1/)[0] : 'Duplicate field';
+        return reply.status(400).send({ success: false, message: `Duplicate field value: ${value}. Please use another value.` });
+    }
+
     // 1. Handle our new standardized AppError
     if (error instanceof AppError) {
         return reply.status(error.statusCode).send({ success: false, message: error.message });
