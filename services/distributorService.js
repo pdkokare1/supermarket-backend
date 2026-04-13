ab16196bf9c829757e4c211b732d9cbdd2d0ea76
pdkokare1 <pdkokare1@gmail.com>
2026-04-13 10:43:33 +0530

    Update distributorService.js

--- services/distributorService.js
+++ services/distributorService.js
@@ -2,15 +2,27 @@
 const Distributor = require('../models/Distributor');
 const AppError = require('../utils/AppError');
 const appEvents = require('../utils/eventEmitter'); // Added for event-driven updates
+const cacheUtils = require('../utils/cacheUtils'); // OPTIMIZATION: Added for caching
 
 exports.getAllDistributors = async () => {
-    return await Distributor.find().sort({ name: 1 }).lean();
+    // OPTIMIZATION: Cache distributor list to prevent unnecessary DB reads
+    const CACHE_KEY = 'distributors:all';
+    let distributors = await cacheUtils.getCachedData(CACHE_KEY);
+    
+    if (!distributors) {
+        distributors = await Distributor.find().sort({ name: 1 }).lean();
+        await cacheUtils.setCachedData(CACHE_KEY, distributors, 3600); // Cache for 1 hour
+    }
+    return distributors;
 };
 
 exports.createDistributor = async (name) => {
     try {
         const newDistributor = new Distributor({ name });
         await newDistributor.save();
+
+        // OPTIMIZATION: Invalidate cache when new distributor is added
+        await cacheUtils.deleteKey('distributors:all');
 
         // EVENT: Notify system of new distributor
         appEvents.emit('DISTRIBUTOR_ADDED', { distributorId: newDistributor._id });
@@ -36,6 +48,9 @@ exports.processPayment = async (distributorId, payload) => {
     });
 
     await distributor.save();
+
+    // OPTIMIZATION: Invalidate cache because financials have updated
+    await cacheUtils.deleteKey('distributors:all');
 
     // EVENT: Notify system of ledger update
     appEvents.emit('DISTRIBUTOR_UPDATED', { distributorId: distributor._id });
