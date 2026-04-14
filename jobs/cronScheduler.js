3cf36a26edb2752daa5d0ddf24a1d2ea83331d13
pdkokare1 <pdkokare1@gmail.com>
2026-04-14 20:00:17 +0530

    Update cronScheduler.js

--- jobs/cronScheduler.js
+++ jobs/cronScheduler.js
@@ -5,28 +5,37 @@ const tasks = require('./cronTasks');
 
 module.exports = function(fastify, updateInventoryReport) {
     
-    cron.schedule('0 0 * * *', () => {
+    // OPTIMIZATION: Track all cron tasks so they can be halted during server drain
+    const scheduledTasks = [];
+
+    scheduledTasks.push(cron.schedule('0 0 * * *', () => {
         tasks.runWithLock('ExpiryWastageMonitor', fastify, () => tasks.runExpiryMonitor(fastify));
-    });
+    }));
 
-    cron.schedule('0 3 * * *', () => {
+    scheduledTasks.push(cron.schedule('0 3 * * *', () => {
         tasks.runWithLock('DataRetentionCleanup', fastify, () => tasks.runDataRetentionCleanup(fastify));
-    });
+    }));
 
-    cron.schedule('0 6 * * *', () => {
+    scheduledTasks.push(cron.schedule('0 6 * * *', () => {
         tasks.runWithLock('RoutineDeliveries', fastify, () => tasks.runRoutineDeliveries(fastify));
-    });
+    }));
 
-    cron.schedule('0 9 * * *', () => {
+    scheduledTasks.push(cron.schedule('0 9 * * *', () => {
         tasks.runWithLock('DailyInventory', fastify, () => tasks.runDailyInventory(fastify, updateInventoryReport));
-    });
+    }));
 
-    cron.schedule('59 23 * * *', () => {
+    scheduledTasks.push(cron.schedule('59 23 * * *', () => {
         tasks.runWithLock('EODBackup', fastify, () => tasks.runEODBackup(fastify));
-    });
+    }));
 
-    cron.schedule('0 2 * * *', () => {
+    scheduledTasks.push(cron.schedule('0 2 * * *', () => {
         tasks.runWithLock('CloudinaryCleanup', fastify, () => tasks.runCloudinaryCleanup(fastify));
-    });
+    }));
     
+    // OPTIMIZATION: Graceful shutdown integration. Stops cron triggers while server is draining.
+    fastify.addHook('onClose', async (instance, done) => {
+        instance.log.info('Halting all scheduled CRON tasks for graceful shutdown...');
+        scheduledTasks.forEach(task => task.stop());
+        done();
+    });
 };
