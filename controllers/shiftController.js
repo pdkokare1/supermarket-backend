716706c609aa0d8ecb24aec7a0fd1b9819602370
pdkokare1 <pdkokare1@gmail.com>
2026-04-15 14:20:43 +0530

    Update shiftController.js

--- controllers/shiftController.js
+++ controllers/shiftController.js
@@ -1,20 +1,19 @@
 /* controllers/shiftController.js */
 
 const shiftService = require('../services/shiftService');
-const catchAsync = require('../utils/catchAsync');
 
-exports.openShift = catchAsync(async (request, reply) => {
+exports.openShift = async (request, reply) => {
     const newShift = await shiftService.openShift(request.body, request.user, request.server.log.error.bind(request.server.log));
 
     return { success: true, data: newShift, message: 'Register Opened Successfully!' };
-}, 'opening shift');
+};
 
-exports.getCurrentShift = catchAsync(async (request, reply) => {
+exports.getCurrentShift = async (request, reply) => {
     const currentShift = await shiftService.getCurrentShift();
     return { success: true, data: currentShift || null };
-}, 'fetching shift');
+};
 
-exports.closeShift = catchAsync(async (request, reply) => {
+exports.closeShift = async (request, reply) => {
     const { shift, discrepancy } = await shiftService.closeShift(request.body, request.user, request.server.log.error.bind(request.server.log));
 
     return { 
@@ -23,4 +22,4 @@ exports.closeShift = catchAsync(async (request, reply) => {
         data: shift,
         discrepancy: discrepancy 
     };
-}, 'closing shift');
+};
