85f04f73fbb72946b3c8432f601a595f4d64d8e5
pdkokare1 <pdkokare1@gmail.com>
2026-04-15 17:23:58 +0530

    Update eventEmitter.js

--- utils/eventEmitter.js
+++ utils/eventEmitter.js
@@ -2,13 +2,29 @@
 'use strict';
 
 const EventEmitter = require('events');
+const cacheUtils = require('./cacheUtils');
 
 /**
  * CENTRAL EVENT BUS
  * This allows different parts of the application to communicate without
  * being directly coupled to each other.
  */
-class AppEventEmitter extends EventEmitter {}
+class AppEventEmitter extends EventEmitter {
+    // OPTIMIZATION: Centralized method to publish across horizontal instances via Redis Pub/Sub
+    broadcastEvent(eventName, payload) {
+        // Wrapped in setImmediate to completely decouple from the main synchronous execution thread
+        setImmediate(() => {
+            // Retain native Node emitter for single-instance listeners
+            this.emit(eventName, payload);
+            
+            // Broadcast to other Railway instances via Redis
+            const redis = cacheUtils.getClient();
+            if (redis) {
+                redis.publish('DAILYPICK_ORDER_EVENTS', JSON.stringify({ eventName, payload })).catch(() => {});
+            }
+        });
+    }
+}
 
 const appEvents = new AppEventEmitter();
 
