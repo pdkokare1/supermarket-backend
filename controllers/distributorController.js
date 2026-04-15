80fee7155f2764c9f1799337fcd54534bd104c0c
pdkokare1 <pdkokare1@gmail.com>
2026-04-15 14:35:26 +0530

    Update distributorController.js

--- controllers/distributorController.js
+++ controllers/distributorController.js
@@ -1,21 +1,20 @@
 /* controllers/distributorController.js */
 
 const distributorService = require('../services/distributorService');
-const catchAsync = require('../utils/catchAsync');
 
-exports.getDistributors = catchAsync(async (request, reply) => {
+exports.getDistributors = async (request, reply) => {
     const distributors = await distributorService.getAllDistributors();
     return { success: true, count: distributors.length, data: distributors };
-}, 'fetching distributors');
+};
 
-exports.createDistributor = catchAsync(async (request, reply) => {
+exports.createDistributor = async (request, reply) => {
     const newDistributor = await distributorService.createDistributor(request.body.name);
     // REMOVED: request.server.broadcastToPOS (Now handled by Service events)
     return { success: true, message: 'Distributor added', data: newDistributor };
-}, 'creating distributor');
+};
 
-exports.processPayment = catchAsync(async (request, reply) => {
+exports.processPayment = async (request, reply) => {
     const distributor = await distributorService.processPayment(request.params.id, request.body);
     // REMOVED: request.server.broadcastToPOS (Now handled by Service events)
     return { success: true, message: 'Payment logged successfully', data: distributor };
-}, 'processing payment');
+};
