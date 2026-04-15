b9e53ed9a46dec4071d39ee62fb62149de717ea4
pdkokare1 <pdkokare1@gmail.com>
2026-04-15 14:09:29 +0530

    Update storeController.js

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,14 +1,13 @@
 /* controllers/storeController.js */
 
 const storeService = require('../services/storeService');
-const catchAsync = require('../utils/catchAsync');
 
-exports.getStores = catchAsync(async (request, reply) => {
+exports.getStores = async (request, reply) => {
     const stores = await storeService.getAllActiveStores();
     return { success: true, data: stores };
-}, 'fetching stores');
+};
 
-exports.createStore = catchAsync(async (request, reply) => {
+exports.createStore = async (request, reply) => {
     const newStore = await storeService.createStore(request.body);
     return { success: true, message: 'Store created successfully', data: newStore };
-}, 'creating store');
+};
