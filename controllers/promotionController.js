9f8b2ff21466c87638223e934100f696b7bfef49
pdkokare1 <pdkokare1@gmail.com>
2026-04-15 14:21:31 +0530

    Update promotionController.js

--- controllers/promotionController.js
+++ controllers/promotionController.js
@@ -1,21 +1,20 @@
 /* controllers/promotionController.js */
 
 const promotionService = require('../services/promotionService');
-const catchAsync = require('../utils/catchAsync');
 
-exports.getPromotions = catchAsync(async (request, reply) => {
+exports.getPromotions = async (request, reply) => {
     const promos = await promotionService.getPromotions(request.query.all);
     return { success: true, count: promos.length, data: promos };
-}, 'fetching promotions');
+};
 
-exports.createPromotion = catchAsync(async (request, reply) => {
+exports.createPromotion = async (request, reply) => {
     const newPromo = await promotionService.createPromotion(request.body);
     // REMOVED: request.server.broadcastToPOS (Now handled by Service events)
     return { success: true, message: 'Promotion created', data: newPromo };
-}, 'creating promotion');
+};
 
-exports.togglePromotion = catchAsync(async (request, reply) => {
+exports.togglePromotion = async (request, reply) => {
     const promo = await promotionService.togglePromotion(request.params.id);
     // REMOVED: request.server.broadcastToPOS (Now handled by Service events)
     return { success: true, message: 'Promotion toggled', data: promo };
-}, 'toggling promotion');
+};
