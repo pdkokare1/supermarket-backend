04f4474ddae6a3b7e63d23d0404d9725c60e7ac1
pdkokare1 <pdkokare1@gmail.com>
2026-04-15 14:13:32 +0530

    Update analyticsController.js

--- controllers/analyticsController.js
+++ controllers/analyticsController.js
@@ -1,7 +1,6 @@
 /* controllers/analyticsController.js */
 
 const analyticsService = require('../services/analyticsService');
-const catchAsync = require('../utils/catchAsync');
 const cacheUtils = require('../utils/cacheUtils');
 
 // MODULARITY: Standardized helper for caching analytics responses
@@ -17,35 +16,35 @@ const respondWithCache = async (reply, cacheKey, ttl, fetchFn) => {
 };
 
 // ENTERPRISE OPTIMIZATION: Endpoint to retrieve core platform growth metrics
-exports.getPlatformGrowthMetrics = catchAsync(async (request, reply) => {
+exports.getPlatformGrowthMetrics = async (request, reply) => {
     const cacheKey = 'analytics:dailypick:growth_metrics';
     return await respondWithCache(reply, cacheKey, 3600, () => 
         analyticsService.getDailyPickGrowthMetrics()
     );
-}, 'calculating strategic growth metrics');
+};
 
-exports.getPnl = catchAsync(async (request, reply) => {
+exports.getPnl = async (request, reply) => {
     // OPTIMIZATION: Relies on the underlying Materialized View inside the Service layer
     const cacheKey = cacheUtils.generateKey('analytics:pnl', request.query);
     return await respondWithCache(reply, cacheKey, 900, () => 
         analyticsService.getPnl(request.query.startDate, request.query.endDate)
     );
-}, 'calculating P&L');
+};
 
-exports.getForecast = catchAsync(async (request, reply) => {
+exports.getForecast = async (request, reply) => {
     const cacheKey = 'analytics:forecast:latest';
     return await respondWithCache(reply, cacheKey, 1800, () => 
         analyticsService.generateForecast(process.env.GEMINI_API_KEY)
     );
-}, 'AI Engine generating forecast');
+};
 
-exports.getLeaderboard = catchAsync(async (request, reply) => {
+exports.getLeaderboard = async (request, reply) => {
     const cacheKey = 'analytics:leaderboard';
     return await respondWithCache(reply, cacheKey, 300, () => 
         analyticsService.getLeaderboard()
     );
-}, 'fetching leaderboard');
+};
 
-exports.getOrdersAnalytics = catchAsync(async (request, reply) => {
+exports.getOrdersAnalytics = async (request, reply) => {
     return await analyticsService.getAnalyticsData();
-}, 'fetching order analytics');
+};
