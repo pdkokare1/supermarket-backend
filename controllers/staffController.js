64a58952db99948158007fbec23d881e56ca70e2
pdkokare1 <pdkokare1@gmail.com>
2026-04-15 14:18:38 +0530

    Update staffController.js

--- controllers/staffController.js
+++ controllers/staffController.js
@@ -1,17 +1,16 @@
 /* controllers/staffController.js */
 
 const staffService = require('../services/staffService');
-const catchAsync = require('../utils/catchAsync');
 
-exports.createStaff = catchAsync(async (request, reply) => {
+exports.createStaff = async (request, reply) => {
     const staffData = await staffService.createStaff(request.body);
     
     // REMOVED: request.server.broadcastToPOS (Now handled by Service events)
 
     return { success: true, message: 'User created successfully.', data: staffData };
-}, 'during user creation');
+};
 
-exports.getStaff = catchAsync(async (request, reply) => {
+exports.getStaff = async (request, reply) => {
     const staff = await staffService.getAllStaff();
     return { success: true, data: staff };
-}, 'fetching staff');
+};
