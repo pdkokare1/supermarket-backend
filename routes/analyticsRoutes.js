2c9bc877b8f6c6f97d285a41ab6d045a3dcb7d84
pdkokare1 <pdkokare1@gmail.com>
2026-04-18 13:01:20 +0530

    Update analyticsRoutes.js

--- routes/analyticsRoutes.js
+++ routes/analyticsRoutes.js
@@ -3,7 +3,8 @@ const analyticsController = require('../controllers/analyticsController');
 
 async function analyticsRoutes(fastify, options) {
     fastify.get('/api/analytics/pnl', { preHandler: [fastify.authenticate, fastify.verifyAdmin] }, analyticsController.getPnl);
-    fastify.post('/api/analytics/forecast', { preHandler: [fastify.authenticate, fastify.verifyAdmin] }, analyticsController.getForecast);
+    // CHANGED: fastify.post to fastify.get to match the frontend request
+    fastify.get('/api/analytics/forecast', { preHandler: [fastify.authenticate, fastify.verifyAdmin] }, analyticsController.getForecast);
     fastify.get('/api/analytics/leaderboard', { preHandler: [fastify.authenticate, fastify.verifyAdmin] }, analyticsController.getLeaderboard);
 }
 module.exports = analyticsRoutes;
