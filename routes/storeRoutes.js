f87f8c6e1203cffd0af3c2bc781bfbe926852ba2
pdkokare1 <pdkokare1@gmail.com>
2026-04-25 09:33:30 +0530

    Update storeRoutes.js

--- routes/storeRoutes.js
+++ routes/storeRoutes.js
@@ -8,6 +8,10 @@ async function storeRoutes(fastify, options) {
 
     // ADMIN ONLY: Create a new store
     fastify.post('/api/stores', { preHandler: [fastify.authenticate, fastify.verifyAdmin] }, storeController.createStore);
+
+    // --- NEW: TENANT DISPUTE ENDPOINT ---
+    // STORE TENANT ONLY: Raise a dispute for a returned/damaged order to freeze automated payouts
+    fastify.post('/api/stores/dispute', { preHandler: [fastify.authenticate] }, storeController.raiseDispute);
 }
 
 module.exports = storeRoutes;
