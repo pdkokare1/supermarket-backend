f2f50c74b0249e39bc7d25fba8413f5e59a773a9
pdkokare1 <pdkokare1@gmail.com>
2026-04-28 20:34:55 +0530

    Update distributorRoutes.js

--- routes/distributorRoutes.js
+++ routes/distributorRoutes.js
@@ -1,12 +1,20 @@
 /* routes/distributorRoutes.js */
+'use strict';
+
 const distributorController = require('../controllers/distributorController');
 
-const distributorSchema = { schema: { body: { type: 'object', required: ['name'], properties: { name: { type: 'string' } } } } };
-const distributorPaymentSchema = { schema: { body: { type: 'object', required: ['amount'], properties: { amount: { type: 'number', minimum: 1 }, paymentMode: { type: 'string' }, referenceNote: { type: 'string' } } } } };
-
 async function distributorRoutes(fastify, options) {
-    fastify.get('/api/distributors', { preHandler: [fastify.authenticate] }, distributorController.getDistributors);
-    fastify.post('/api/distributors', { preHandler: [fastify.authenticate, fastify.verifyAdmin], ...distributorSchema }, distributorController.createDistributor);
-    fastify.post('/api/distributors/:id/pay', { preHandler: [fastify.authenticate, fastify.verifyAdmin], ...distributorPaymentSchema }, distributorController.processPayment);
+    // Existing Routes
+    fastify.get('/api/distributors', { preHandler: [fastify.authenticate] }, distributorController.getAllDistributors);
+    fastify.post('/api/distributors', { preHandler: [fastify.authenticate, fastify.verifyAdmin] }, distributorController.createDistributor);
+    fastify.post('/api/distributors/:id/pay', { preHandler: [fastify.authenticate, fastify.verifyAdmin] }, distributorController.logPayment);
+
+    // --- NEW: PHASE 3 B2B PORTAL ROUTES ---
+    // Endpoint for distributors to pull their incoming orders from local shops
+    fastify.get('/api/distributors/:id/orders', { preHandler: [fastify.authenticate] }, distributorController.getDistributorPOs);
+    
+    // Endpoint for distributors to update the dispatch status of a B2B order
+    fastify.put('/api/distributors/orders/:orderId/status', { preHandler: [fastify.authenticate] }, distributorController.updatePurchaseOrderStatus);
 }
+
 module.exports = distributorRoutes;
